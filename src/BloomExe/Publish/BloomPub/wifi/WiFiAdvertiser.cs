a93c1316eced7d40c36a392dbc5f8c3a5e65adfc
Wade Mergenthal <wmergenthal@gmail.com>
2024-01-29 15:04:45 -0500

    Same implementation of BR -> BD request over TCP instead of UDP from work based on 'master' (which is alpha) but now based on 'Version5.6' (which is beta). Re-established the workspace in this way at the suggestion of John Hatton to work around an alpha bug that would not accept the Bloom Enterprise subscription account number for development. This caused BD to not allow my book to be shared via wifi.

--- src/BloomExe/Publish/BloomPub/wifi/WiFiAdvertiser.cs
+++ src/BloomExe/Publish/BloomPub/wifi/WiFiAdvertiser.cs
@@ -7,157 +7,185 @@ using System.Text;
 using System.Threading;
 using Bloom.Api;
 using Bloom.web;
-using SIL.Progress;
+//using SIL.Progress;
 
 namespace Bloom.Publish.BloomPub.wifi
 {
-	/// <summary>
-	/// This class broadcasts a message over the network offering to supply a book to any Android that wants it.
-	/// </summary>
-	public class WiFiAdvertiser : IDisposable
-	{
-		// The information we will advertise.
-		public string BookTitle;
-		private string _bookVersion;
+    /// <summary>
+    /// This class broadcasts a message over the network offering to supply a book to any Android that wants it.
+    /// </summary>
+    public class WiFiAdvertiser : IDisposable
+    {
+        // The information we will advertise.
+        public string BookTitle;
+        private string _bookVersion;
 
-		public string BookVersion
-		{
-			get { return _bookVersion; }
-			set
-			{
-				_bookVersion = value;
-				// In case this gets modified after we start advertising, we need to recompute the advertisement
-				// next time we send it. Clearing this makes sure it happens.
-				_currentIpAddress = "";
-			}
-		}
-		public string TitleLanguage;
+        public string BookVersion
+        {
+            get { return _bookVersion; }
+            set
+            {
+                _bookVersion = value;
+                // In case this gets modified after we start advertising, we need to recompute the advertisement
+                // next time we send it. Clearing this makes sure it happens.
+                _currentIpAddress = "";
+            }
+        }
+        public string TitleLanguage;
 
-		private UdpClient _client;
-		private Thread _thread;
-		private IPEndPoint _endPoint;
-		// The port on which we advertise.
-		// ChorusHub uses 5911 to advertise. Bloom looks for a port for its server at 8089 and 10 following ports.
-		// https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers shows a lot of ports in use around 8089,
-		// but nothing between 5900 and 5931. Decided to use a number similar to ChorusHub.
-		private const int Port = 5913; // must match port in BloomReader NewBookListenerService.startListenForUDPBroadcast
-		private string _currentIpAddress;
-		private byte[] _sendBytes; // Data we send in each advertisement packet
-		private readonly WebSocketProgress _progress;
+        private UdpClient _client;
+        private Thread _thread;
+        private IPEndPoint _endPoint;
 
-		internal WiFiAdvertiser(WebSocketProgress progress)
-		{
-			_progress = progress;
-		}
+        // The port on which we advertise.
+        // ChorusHub uses 5911 to advertise. Bloom looks for a port for its server at 8089 and 10 following ports.
+        // https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers shows a lot of ports in use around 8089,
+        // but nothing between 5900 and 5931. Decided to use a number similar to ChorusHub.
+        private const int Port = 5913; // must match port in BloomReader NewBookListenerService.startListenForUDPBroadcast
+        private string _currentIpAddress;
+        private byte[] _sendBytes; // Data we send in each advertisement packet
+        private readonly WebSocketProgress _progress;
 
-		public void Start()
-		{
-			// The doc seems to indicate that EnableBroadcast is required for doing broadcasts.
-			// In practice it seems to be required on Mono but not on Windows.
-			// This may be fixed in a later version of one platform or the other, but please
-			// test both if tempted to remove it.
-			_client = new UdpClient
-			{
-				EnableBroadcast = true
-			};
-			_endPoint = new IPEndPoint(IPAddress.Parse("255.255.255.255"), Port);
-			_thread = new Thread(Work);
-			_thread.Start();
-		}
+        internal WiFiAdvertiser(WebSocketProgress progress)
+        {
+            _progress = progress;
+        }
 
-		public bool Paused { get; set; }
+        public void Start()
+        {
+            // The doc seems to indicate that EnableBroadcast is required for doing broadcasts.
+            // In practice it seems to be required on Mono but not on Windows.
+            // This may be fixed in a later version of one platform or the other, but please
+            // test both if tempted to remove it.
+            _client = new UdpClient { EnableBroadcast = true };
+            _endPoint = new IPEndPoint(IPAddress.Parse("255.255.255.255"), Port);
+            _thread = new Thread(Work);
+            Debug.WriteLine("WM, WiFiAdvertiser::Start, work thread start"); // WM, temporary
+            _thread.Start();
+        }
 
-		private void Work()
-		{
-			_progress.Message(idSuffix: "beginAdvertising", message: "Advertising book to Bloom Readers on local network...");
-			try
-			{
-				while (true)
-				{
-					if (!Paused)
-					{
-						UpdateAdvertisementBasedOnCurrentIpAddress();
-						_client.BeginSend(_sendBytes, _sendBytes.Length, _endPoint, SendCallback, _client);
-					}
-					Thread.Sleep(1000);
-				}
-			}
-			catch (ThreadAbortException)
-			{
-				_progress.Message(idSuffix: "Stopped", message: "Stopped Advertising.");
-				_client.Close();
-			}
-			catch (Exception error)
-			{
-				// not worth localizing
-				_progress.MessageWithoutLocalizing($"Error in Advertiser: {error.Message}", ProgressKind.Error);
-			}
-		}
-		public static void SendCallback(IAsyncResult args)
-		{
-		}
+        public bool Paused { get; set; }
 
-		/// <summary>
-		/// Since this is typically not a real "server", its ipaddress could be assigned dynamically,
-		/// and could change each time someone wakes it up.
-		/// </summary>
-		private void UpdateAdvertisementBasedOnCurrentIpAddress()
-		{
-			if (_currentIpAddress != GetLocalIpAddress())
-			{
-				_currentIpAddress = GetLocalIpAddress();
-				dynamic advertisement = new DynamicJson();
-				advertisement.title = BookTitle;
-				advertisement.version = BookVersion;
-				advertisement.language = TitleLanguage;
-				advertisement.protocolVersion = WiFiPublisher.ProtocolVersion;
-				advertisement.sender = System.Environment.MachineName;
+        private void Work()
+        {
+            _progress.Message(
+                idSuffix: "beginAdvertising",
+                message: "Advertising book to Bloom Readers on local network..."
+            );
+            Debug.WriteLine("WM, WiFiAdvertiser::Work, begin UDP advertising loop"); // WM, temporary
+            try
+            {
+                while (true)
+                {
+                    if (!Paused)
+                    {
+                        UpdateAdvertisementBasedOnCurrentIpAddress();
+                        Debug.WriteLine("WM, WiFiAdvertiser::Work, sending"); // WM, temporary
+                        _client.BeginSend(
+                            _sendBytes,
+                            _sendBytes.Length,
+                            _endPoint,
+                            SendCallback,
+                            _client
+                        );
+                    }
+                    Debug.WriteLine("WM, WiFiAdvertiser::Work, 1-second sleep"); // WM, temporary
+                    Thread.Sleep(1000);
+                }
+            }
+            catch (ThreadAbortException)
+            {
+                _progress.Message(idSuffix: "Stopped", message: "Stopped Advertising.");
+                Debug.WriteLine("WM, WiFiAdvertiser::Work, shutting down client"); // WM, temporary
+                _client.Close();
+            }
+            catch (Exception error)
+            {
+                // not worth localizing
+                _progress.MessageWithoutLocalizing(
+                    $"Error in Advertiser: {error.Message}",
+                    ProgressKind.Error
+                );
+            }
+        }
 
-				_sendBytes = Encoding.UTF8.GetBytes(advertisement.ToString());
-				//EventLog.WriteEntry("Application", "Serving at http://" + _currentIpAddress + ":" + ChorusHubOptions.MercurialPort, EventLogEntryType.Information);
-			}
-		}
+        public static void SendCallback(IAsyncResult args) { }
 
-		/// <summary>
-		/// The intent here is to get an IP address by which this computer can be found on the local subnet.
-		/// This is ambiguous if the computer has more than one IP address (typically for an Ethernet and WiFi adapter).
-		/// Early experiments indicate that things work whichever one is used, assuming the networks are connected.
-		/// Eventually we may want to prefer WiFi if available (see code in HearThis), or even broadcast on all of them.
-		/// </summary>
-		/// <returns></returns>
-		private string GetLocalIpAddress()
-		{
-			string localIp = null;
-			var host = Dns.GetHostEntry(Dns.GetHostName());
+        /// <summary>
+        /// Since this is typically not a real "server", its ipaddress could be assigned dynamically,
+        /// and could change each time someone wakes it up.
+        /// </summary>
+        private void UpdateAdvertisementBasedOnCurrentIpAddress()
+        {
+            //Debug.WriteLine("WM, WiFiAdvertiser::UABOCIA, begin, _currentIpAddress = " + _currentIpAddress); // WM, temporary
+            if (_currentIpAddress != GetLocalIpAddress())
+            {
+                _currentIpAddress = GetLocalIpAddress();
+                //Debug.WriteLine("WM, WiFiAdvertiser::UABOCIA, updated, _currentIpAddress = " + _currentIpAddress); // WM, temporary
+                dynamic advertisement = new DynamicJson();
+                advertisement.title = BookTitle;
+                advertisement.version = BookVersion;
+                advertisement.language = TitleLanguage;
+                advertisement.protocolVersion = WiFiPublisher.ProtocolVersion;
+                advertisement.sender = System.Environment.MachineName;
 
-			foreach (var ipAddress in host.AddressList.Where(ipAddress => ipAddress.AddressFamily == AddressFamily.InterNetwork))
-			{
-				if (localIp != null)
-				{
-					if (host.AddressList.Length > 1)
-					{
-						//EventLog.WriteEntry("Application", "Warning: this machine has more than one IP address", EventLogEntryType.Warning);
-					}
-				}
-				localIp = ipAddress.ToString();
-			}
-			return localIp ?? "Could not determine IP Address!";
-		}
+                _sendBytes = Encoding.UTF8.GetBytes(advertisement.ToString());
+                //EventLog.WriteEntry("Application", "Serving at http://" + _currentIpAddress + ":" + ChorusHubOptions.MercurialPort, EventLogEntryType.Information);
+            }
+        }
 
-		public void Stop()
-		{
-			if (_thread == null)
-				return;
+        /// <summary>
+        /// The intent here is to get an IP address by which this computer can be found on the local subnet.
+        /// This is ambiguous if the computer has more than one IP address (typically for an Ethernet and WiFi adapter).
+        /// Early experiments indicate that things work whichever one is used, assuming the networks are connected.
+        /// Eventually we may want to prefer WiFi if available (see code in HearThis), or even broadcast on all of them.
+        ///
+        /// See https://stackoverflow.com/questions/6803073/get-local-ip-address/27376368#27376368 for a way
+        /// to determine which of potentially multiple IP addresses will be the one used. The technique is used
+        /// successfully in BloomReaderTCPListener::ListenForTCPMessages(), whose development was done with a
+        /// laptop that had 9 IP addresses (comprising a mix of IPv4 and IPv6). In that development the function
+        /// below always returned an address different from the one actually being used.
+        /// </summary>
+        /// <returns></returns>
+        private string GetLocalIpAddress()
+        {
+            string localIp = null;
+            var host = Dns.GetHostEntry(Dns.GetHostName());
 
-			//EventLog.WriteEntry("Application", "Advertiser Stopping...", EventLogEntryType.Information);
-			_thread.Abort();
-			_thread.Join(2 * 1000);
-			_thread = null;
-		}
+            foreach (
+                var ipAddress in host.AddressList.Where(
+                    ipAddress => ipAddress.AddressFamily == AddressFamily.InterNetwork
+                )
+            )
+            {
+                if (localIp != null)
+                {
+                    if (host.AddressList.Length > 1)
+                    {
+                        //EventLog.WriteEntry("Application", "Warning: this machine has more than one IP address", EventLogEntryType.Warning);
+                    }
+                }
+                localIp = ipAddress.ToString();
+            }
+            Debug.WriteLine("WM, WiFiAdvertiser::GetLocalIpAddress, returning localIp = " + localIp); // WM, temporary
+            return localIp ?? "Could not determine IP Address!";
+        }
 
-		public void Dispose()
-		{
-			Stop();
-		}
-	}
+        public void Stop()
+        {
+            if (_thread == null)
+                return;
+
+            //EventLog.WriteEntry("Application", "Advertiser Stopping...", EventLogEntryType.Information);
+            Debug.WriteLine("WM, WiFiAdvertiser::Stop, work thread stop"); // WM, temporary
+            _thread.Abort();
+            _thread.Join(2 * 1000);
+            _thread = null;
+        }
+
+        public void Dispose()
+        {
+            Stop();
+        }
+    }
 }
